2462979f0e03313b9b50faeaef6ec5bd28b3d1a3
unknown <irena.sturua@gmail.com>
2024-02-09 18:09:44 +0400

    Add code.php

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>LuxVentus</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">     
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap">  
+    
+    <?php require "code.php"; ?>
+
 </head>
 <body>
     <div class="body">
